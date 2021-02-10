eba544c1d8c8731331f31793ce26cb7134a27fa2
Gabriel Guimaraes <dealmeida.gabriel.d@gmail.com>
2021-02-09 22:46:48 -0300

    Finalizando.

--- src/pages/home/home.page.js
+++ src/pages/home/home.page.js
@@ -2,8 +2,14 @@ import React from "react";
 import {
     Text,
     View,
+    Linking
 } from "react-native";
 import {CustomHeaderComponent} from "../../components/custom-header/custom-header.component";
+import {AlignStyle} from "../../style/align.style";
+import {FlexStyle} from "../../style/flex.style";
+import {PaddingStyle} from "../../style/padding.style";
+import {TextStyle} from "../../style/text.style";
+import {MarginStyle} from "../../style/margin.style";
 
 export class HomePage extends React.Component {
     constructor(props) {
@@ -16,8 +22,13 @@ export class HomePage extends React.Component {
         return(
             <>
                 <CustomHeaderComponent drawerNavigation={this.props.navigation}/>
-                <View>
-                    <Text>Home</Text>
+                <View style={[FlexStyle.makeFlex(9), AlignStyle.centerX, PaddingStyle.makePadding(5,10,5,10)]}>
+                    <Text style={[TextStyle.makeFontSize(25), TextStyle.textDecoration.negrito]}>Wellcome!</Text>
+                    <Text style={[TextStyle.makeFontSize(15)]}>Select a http request type  and enjoy!!</Text>
+                </View>
+                <View style={[FlexStyle.makeFlex(1), AlignStyle.centerX, PaddingStyle.makePadding(5,10,5,10)]}>
+                    <Text style={[TextStyle.makeFontSize(15), MarginStyle.makeMargin(0,0,0,10)]}>Made by Gabriel Guimarães de Almeida</Text>
+                    <Text style={[TextStyle.makeFontSize(13), TextStyle.textColor.azul]} onPress={() => Linking.openURL('https://github.com/dealmeida-gabriel-1945/DearRequest')}>Access the Github repository here!</Text>
                 </View>
             </>
         );
